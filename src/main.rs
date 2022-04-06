338f278c466255a90193adc5b6a060a575bd1201
vlaroque <vlaroque42@gmail.com>
2022-04-06 19:13:57 +0200

    deleted useless file

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,31 @@
 mod onemacro;
-mod lineparser;
-use onemacro::OneMacro;
+use clap::{arg, command};
 use colored::*;
+use onemacro::OneMacro;
 use regex::Regex;
 use std::collections::HashMap;
 use std::fs;
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
-use clap::{arg, command, Command, ArgMatches};
 
 fn arg_parse() -> Option<(String, String)> {
     let mut in_filename = String::new();
     let mut out_filename = in_filename.to_string();
     let matches = command!()
         .arg(arg!([filename] "markdown file name to compile").required(true))
-        .arg(arg!(-o --output <FILE> "Sets a custom output file")
-            .required(false)
-        )
+        .arg(arg!(-o --output <FILE> "Sets a custom output file").required(false))
         .get_matches();
 
     if let Some(filename) = matches.value_of("filename") {
         in_filename = filename.to_string();
     }
 
+    let in_file_path = Path::new(matches.value_of("filename").expect("fail"));
+
     if let Some(filename) = matches.value_of("output") {
         out_filename = filename.to_string();
-    }
-    else {
+    } else {
         out_filename = in_filename.clone();
         out_filename.push_str(".md");
     }
@@ -52,14 +50,14 @@ fn main() {
     let macro_regex = Regex::new(r"![A-Z]*\(.*\)[ ]*\{.*\}").unwrap();
     let lines = content.lines();
     let mut macro_list: HashMap<String, onemacro::OneMacro> = HashMap::new();
-    
+
     println!("WTF {:?}", out_filename);
     let mut file = File::create(&out_filename).expect("failed openfile");
 
     for line in lines {
         let mut line: String = String::from(line);
         println!("{}: {}", "LINE".bright_yellow(), line);
-        line = lineparser::parseline(&line, &macro_list);
+        line = onemacro::parseline(&line, &macro_list);
 
         if macro_regex.is_match(&line) {
             println!("   >>> MACRO DECLARATION!");
@@ -74,5 +72,4 @@ fn main() {
         println!("{}: {}", "RESULT".bright_green(), line);
     }
     println!("\n\n   ====>>> MACRO LIST {:?}", macro_list);
-    
 }
--- src/onemacro.rs
+++ src/onemacro.rs
@@ -53,42 +53,96 @@ fn from_args_to_vec(args: &str) -> Vec<String> {
     args.iter().map(|s| s.trim().to_string()).collect()
 }
 
-pub fn process_string(string: &mut String, macro_list: &HashMap<String, OneMacro>) {
-    let regex = Regex::new(r"([^!A-Z]|^)(?P<name>[A-Z]+)\((?P<args>.*?)\)").unwrap();
+#[derive(PartialEq)]
+enum ParsingState {
+    Normal,
+    MacroName,
+    NoMacroName,
+    WaintinClosePar,
+}
 
-    loop {
-        println!("new loop {}", string);
-        let mut working_string = string.clone();
-        let capture = match regex.captures(&working_string) {
-            Some(cap) => cap,
-            None => {
-                println!("found none");
-                break;
-            }
-        };
-        let matching_string = &capture[0].trim();
-        let macro_name = &capture["name"];
-        let args: Vec<String> = from_args_to_vec(&capture["args"]);
-
-        if !macro_list.contains_key(macro_name) {
-            break;
-        }
-
-        let macro_pattern: &OneMacro = macro_list
-            .get(macro_name)
-            .expect("wtf macro should have been there");
-
-        let mut macro_string = macro_pattern.body.clone();
-        for (argument, param) in args.iter().zip(macro_pattern.arguments.iter()) {
-            macro_string = macro_string.replace(param, argument);
-            println!(
-                "!!! '{}' => '{}' == {}",
-                param.red(),
-                argument.green(),
-                macro_string
-            );
-            *string = working_string.replace(matching_string, &macro_string);
-        }
-        println!("end loop {}", string);
+fn push_if_need(vector: &mut Vec<String>, string: &mut String) {
+    if !string.is_empty() {
+        vector.push(string.clone());
+        string.clear();
     }
+}
+
+pub fn parseline(string_to_parse: &String, macro_map: &HashMap<String, OneMacro>) -> String {
+    let mut final_string: Vec<String> = vec![];
+    let mut temp_string: String = String::new();
+    let mut open_parentheses_count = 0;
+    let mut state = ParsingState::Normal;
+    let mut macro_name = String::new();
+
+    for letter in string_to_parse.chars() {
+        match state {
+            ParsingState::Normal => {
+                if letter.is_uppercase() {
+                    state = ParsingState::MacroName;
+                    push_if_need(&mut final_string, &mut temp_string);
+                } else if letter == '!' {
+                    state = ParsingState::NoMacroName;
+                }
+            }
+            ParsingState::MacroName => {
+                if letter == '(' {
+                    if macro_map.contains_key(&temp_string) {
+                        state = ParsingState::WaintinClosePar;
+                        macro_name = temp_string.clone(); // ici on sauvegarde le nom de la macro
+                        temp_string.clear();
+                        // final_string.push("(".to_string()); //a sauvegarder au cas ou la macro foire
+                        open_parentheses_count = 1;
+                        continue;
+                    } else {
+                        state = ParsingState::Normal;
+                    }
+                } else if !letter.is_uppercase() && letter != '_' {
+                    state = ParsingState::Normal;
+                }
+            }
+            ParsingState::NoMacroName => {
+                if !letter.is_uppercase() && letter != '_' {
+                    state = ParsingState::Normal;
+                }
+            }
+            ParsingState::WaintinClosePar => {
+                if letter == '(' {
+                    open_parentheses_count += 1
+                } else if letter == ')' {
+                    if open_parentheses_count == 1 {
+                        state = ParsingState::Normal;
+                        // Here we call recursively the same finction on argument to threat them before macro executing
+                        temp_string = parseline(&temp_string, &macro_map);
+                        let current_macro: &OneMacro =
+                            macro_map.get(&macro_name).expect("macro dont exist");
+                        macro_name.clear();
+
+                        temp_string = current_macro
+                            .apply(temp_string.clone())
+                            .expect("not a macro");
+
+                        push_if_need(&mut final_string, &mut temp_string);
+                        //final_string.push(")".to_string());     // il faudra la rajouter si ca foire
+                        continue;
+                    } else if open_parentheses_count > 1 {
+                        open_parentheses_count -= 1;
+                    } else {
+                        panic!("never should happen")
+                    }
+                }
+            }
+        }
+        temp_string.push(letter);
+    }
+    if state == ParsingState::WaintinClosePar {
+        push_if_need(&mut final_string, &mut macro_name);
+        final_string.push("(".to_string());
+        push_if_need(&mut final_string, &mut temp_string);
+    }
+    push_if_need(&mut final_string, &mut temp_string);
+    //println!("PARSER RESULT = {:?}", final_string);
+    let final_string = final_string.concat();
+
+    return final_string;
 }
